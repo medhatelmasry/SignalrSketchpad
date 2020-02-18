b24717747b61a159649c19f2c7c4106437b51e23
Medhat Elmasry <medhat@elmasry.ca>
2020-02-17 19:17:15 -0800

    1st commit

---
+++ Hubs/DrawDotHub.cs
@@ -0,0 +1,18 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace SignalrSketchpad.Hubs
+{
+    public class DrawDotHub : Hub
+    {
+        public async Task UpdateCanvas(int x, int y)
+        {
+            await Clients.All.SendAsync("updateDot", x, y);
+        }
+
+        public async Task ClearCanvas()
+        {
+            await Clients.All.SendAsync("clearCanvas");
+        }
+    }
+}
